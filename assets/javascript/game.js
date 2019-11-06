426caf3eac5a88ba1d25ca0227bfadced7404e44
Francisco Fernandez <francisco.a.fernandez@gmail.com>
2019-11-05 21:02:50 -0500

    added padding to instructions div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,15 @@
 ("#crystal-ball-1").on("click", function(){
     //code to change score goes here
 });
+
+("#crystal-ball-2").on("click", function(){
+    //code to change score goes here
+});
+
+("#crystal-ball-3").on("click", function(){
+    //code to change score goes here
+});
+
+("#crystal-ball-4").on("click", function(){
+    //code to change score goes here
+});
