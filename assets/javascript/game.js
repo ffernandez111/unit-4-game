26a8154e2d66c002acd30655df8f3c7ace527f8a
Francisco Fernandez <francisco.a.fernandez@gmail.com>
2019-11-06 21:32:44 -0500

    aded random number div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,23 +9,36 @@ var yellowBallValue = (Math.floor(Math.random() *10)+6)
 
 
 
-("#crystal-ball-1").on("click", function(){
-  if (score == targetScore  ){
+$("#crystal-ball-1").on("click", function(){
+    console.log("1")
+  //score=blueBallValue + score;
+  console.log(score)
+  if (score == targetScore){
     //wins++ - This is the same as wins = wins + 1
-    wins=wins + 1;
-  }  
+    wins=wins + 1; 
+  }
+  else if(score < targetScore){
+
+  }
+  else {
+      losses=losses +1;
+  }
+    
 });
 
-("#crystal-ball-2").on("click", function(){
+$("#crystal-ball-2").on("click", function(){
     //code to change score goes here
+    console.log("2")
 });
 
-("#crystal-ball-3").on("click", function(){
+$("#crystal-ball-3").on("click", function(){
     //code to change score goes here
+    console.log("3")
 });
 
-("#crystal-ball-4").on("click", function(){
+$("#crystal-ball-4").on("click", function(){
     //code to change score goes here
+    console.log("4")
 });
 
 
