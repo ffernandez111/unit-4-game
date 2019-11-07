7923b5fd739b2a30bfebaea1f33b64ee0bea62bd
Francisco Fernandez <francisco.a.fernandez@gmail.com>
2019-11-06 20:18:32 -0500

    added variable values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,19 @@
-var targetScore
-var wins
-var losses
-var score
-var blueBallValue
-var greenBallValue
-var redBallValue
-var yellowBallValue
-
-
-
-
+var targetScore = (Math.floor(Math.random() *100)+19)
+var wins = 0;
+var losses =0;
+var score =0;
+var blueBallValue = (Math.floor(Math.random() *10)+3)
+var greenBallValue = (Math.floor(Math.random() *10)+4)
+var redBallValue = (Math.floor(Math.random() *10)+5) 
+var yellowBallValue = (Math.floor(Math.random() *10)+6)
 
 
 
 ("#crystal-ball-1").on("click", function(){
-    
+  if (score == targetScore  ){
+    //wins++ - This is the same as wins = wins + 1
+    wins=wins + 1;
+  }  
 });
 
 ("#crystal-ball-2").on("click", function(){
