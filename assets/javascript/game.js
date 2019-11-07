da302053d1077f0664d1c3620356af94f01ec901
Francisco Fernandez <francisco.a.fernandez@gmail.com>
2019-11-06 20:09:02 -0500

    added variables to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,20 @@
+var targetScore
+var wins
+var losses
+var score
+var blueBallValue
+var greenBallValue
+var redBallValue
+var yellowBallValue
+
+
+
+
+
+
+
 ("#crystal-ball-1").on("click", function(){
-    //code to change score goes here
+    
 });
 
 ("#crystal-ball-2").on("click", function(){
@@ -15,29 +30,8 @@
 });
 
 
-var listOfNums = ["one","two","three"]
-var computerGuess = listOfNums[Math.floor(Math.random() * listOfNums.length)];
-var numOfGuesses = computerGuess.length + 8;
-var results = [];
-var blankWord = []; 
-console.log (computerGuess);
-for (var i = 0; i < computerGuess.length; i++) {
-    results[i] = " _ ";
+
+
+
+
     
-}
-    
-    document.onkeyup = function(event) {
-    // Determines which key was pressed.
-    var userGuess = event.key;
-    
-    for (var j = 0; j < computerGuess.length; j++) {
-        // Hit
-        if (userGuess === computerGuess.charAt(j)) {
-            results[j] = userGuess;
-            console.log (results);
-            $("#blankWord").text(results);
-        } else {
-            // Miss
-            console.log('Wrong letter:', userGuess);
-        }
-    }
